59dff16e282764124bb5b4e4e24f90d2970ef526
Nurbakyt <n79907561@gmail.com>
2024-11-14 18:14:06 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./components/header/Header"
+import './app.css'
+import Hero from "./components/hero/Hero"
+import Welcome from "./components/welcome/Welcome"
+
+function App() {
+
+
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <Welcome />
+    </div>
+  )
+}
+
+export default App
